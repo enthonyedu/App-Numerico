b436248ad38cc5bb2b6f89b2f0758ab28e00538e
Enthony <enthonyedu@gmail.com>
2023-06-03 14:00:57 -0300

    feat: adicionado a rota para o botão principal

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:operacoes_numericas/views/home_page.dart';
 
 void main(List<String> args) {
-  runApp(const Myapp());
+  runApp(const MyApp());
 }
 
-class Myapp extends StatelessWidget {
-  const Myapp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'App Númerico',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomePage(),
+      },
     );
   }
 }
