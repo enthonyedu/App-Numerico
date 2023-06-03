b6f23825051497a632e3a3396223506b1ca0e44e
Enthony <enthonyedu@gmail.com>
2023-06-03 13:59:00 -0300

    feat: adicionado o botão na home pag

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:operacoes_numericas/components/app_bar.dart';
+import 'package:operacoes_numericas/components/main_button.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -9,6 +10,20 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: appBar(),
       backgroundColor: Colors.white,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              mainButton(
+                funcao: () {},
+                texto: 'Acessar Calculadora',
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
