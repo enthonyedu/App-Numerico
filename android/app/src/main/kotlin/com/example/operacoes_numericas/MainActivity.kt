0c8b31244c1db10272c27908fecb87c5455322b7
Enthony <enthonyedu@gmail.com>
2023-06-03 11:37:58 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/operacoes_numericas/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.operacoes_numericas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(const Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'App Númerico',
+    );
+  }
+}
