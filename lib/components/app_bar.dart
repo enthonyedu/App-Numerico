44cfcca2865b0709efc1fa1cbd13516e2ab6364a
Enthony <enthonyedu@gmail.com>
2023-06-03 11:46:47 -0300

    feat: adicionado o componente AppBar

---
+++ lib/components/app_bar.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+AppBar appBar() {
+  return AppBar(
+    leading: const Icon(Icons.health_and_safety),
+    title: const Text('Bem-Estar Fit'),
+    backgroundColor: Color.fromARGB(255, 0, 199, 17),
+  );
+}
