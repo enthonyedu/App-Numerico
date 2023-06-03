0262bc0565018bfdffb682af412d607db1f86151
Enthony <enthonyedu@gmail.com>
2023-06-03 16:50:42 -0300

    feat: criado o model de Number

---
+++ lib/model/number.dart
@@ -0,0 +1,21 @@
+class Number {
+  int _value = 0;
+
+  String _type = '';
+
+  int getValue() {
+    return _value;
+  }
+
+  void setValue(value) {
+    _value = value;
+  }
+
+  String getType() {
+    return _type;
+  }
+
+  void setType(type) {
+    _type = type;
+  }
+}
